673857a0c152cadbfbc6cdcb9df43d97b5ea7731
Samet Yilmaz <syilmaz@handpickedagencies.com>
2024-03-11 13:31:57 +0100

    Changes

--- src/handler.py
+++ src/handler.py
@@ -1,17 +1,20 @@
 """ Example handler file. """
 
 import runpod
-from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForImage2Image
 import torch
 import base64
 import io
 import time
+from PIL import Image
+from io import BytesIO
+
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
 
 try:
-    pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+    pipe = AutoPipelineForImage2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
     pipe.to("cuda")
 except RuntimeError:
     quit()
@@ -20,9 +23,12 @@ def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
     prompt = job_input['prompt']
+    image = job_input['image']
+    img = Image.open(BytesIO(base64.b64decode(image))).convert("RGB")
+    img.thumbnail((768, 768))
 
     time_start = time.time()
-    image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    image = pipe(prompt=prompt, num_inference_steps=2, guidance_scale=0.0, image=img).images[0]
     print(f"Time taken: {time.time() - time_start}")
 
     buffer = io.BytesIO()
