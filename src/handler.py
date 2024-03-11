7c89014ff5ab0ea10b6a10d0416bdf3962477ca2
Samet Yilmaz <syilmaz@handpickedagencies.com>
2024-03-11 15:21:55 +0100

    Up

--- src/handler.py
+++ src/handler.py
@@ -9,6 +9,7 @@ import time
 from PIL import Image
 from io import BytesIO
 import requests
+import math
 
 
 # If your handler runs inference on a model, load the model here.
@@ -19,6 +20,13 @@ try:
     pipe.to("cuda")
 except RuntimeError:
     quit()
+
+
+def resize_crop(image, size=512):
+    image = image.convert("RGB")
+    w, h = image.size
+    image = image.resize((size, int(size * (h / w))), Image.BICUBIC)
+    return image
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
@@ -36,12 +44,31 @@ def handler(job):
 
     response = requests.get(url)
     image = Image.open(BytesIO(response.content)).convert("RGB")
-    image.thumbnail((768, 768))
 
-    image = pipe(prompt, image, num_inference_steps=2, strength=0.75, guidance_scale=1.5).images[0]
+    strength = 0.7
+    steps = 2
+    seed = 1231231
+    init_image = resize_crop(image)
+    generator = torch.manual_seed(seed)
+    last_time = time.time()
+
+    if int(steps * strength) < 1:
+        steps = math.ceil(1 / max(0.10, strength))
+        
+    results = pipe(
+        prompt=prompt,
+        image=init_image,
+        generator=generator,
+        num_inference_steps=steps,
+        guidance_scale=0.0,
+        strength=strength,
+        width=512,
+        height=512,
+        output_type="pil",
+    )
 
     buffer = io.BytesIO()
-    image.save(buffer, format="PNG")
+    results.save(buffer, format="PNG")
     image_bytes = buffer.getvalue()
 
     return base64.b64encode(image_bytes).decode('utf-8')
