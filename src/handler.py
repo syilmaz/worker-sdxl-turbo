294fbf7ce077818f87a32c51eb6e1d3c0c24f3cf
Samet Yilmaz <syilmaz@handpickedagencies.com>
2024-03-12 10:24:28 +0100

    x

--- src/handler.py
+++ src/handler.py
@@ -32,7 +32,7 @@ def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
     prompt = job_input['prompt']
-    # image = job_input['image']
+    img = job_input['image']
     # img = Image.open(BytesIO(base64.b64decode(image))).convert("RGB")
     # img.thumbnail((768, 768))
 
@@ -40,10 +40,10 @@ def handler(job):
     # image = pipe(prompt=prompt, num_inference_steps=2, guidance_scale=0.0, image=img).images[0]
     # print(f"Time taken: {time.time() - time_start}")
 
-    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/1665_Girl_with_a_Pearl_Earring.jpg/800px-1665_Girl_with_a_Pearl_Earring.jpg"
+    # url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/1665_Girl_with_a_Pearl_Earring.jpg/800px-1665_Girl_with_a_Pearl_Earring.jpg"
 
-    response = requests.get(url)
-    image = Image.open(BytesIO(response.content)).convert("RGB")
+    # response = requests.get(url)
+    image = Image.open(BytesIO(base64.b64decode(image))).convert("RGB")
 
     strength = 0.7
     steps = 2
